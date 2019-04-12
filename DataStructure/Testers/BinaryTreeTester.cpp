9fe0c1bea07c01435b1ec01e36e7d250cea0d665
Luis Veliz <luisvelizjr@gmail.com>
2019-04-12 13:54:28 -0600

    stuff

---
+++ DataStructure/Testers/BinaryTreeTester.cpp
@@ -0,0 +1,14 @@
+//
+//  BinaryTreeTester.cpp
+//  DataStructure
+//
+//  Created by Veliz, Luis on 4/12/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "BinaryTreeTester.hpp"
+
+void BinaryTreeTester :: treeStuff()
+{
+    
+}
---
+++ DataStructure/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,25 @@
+//
+//  BinaryTreeTester.hpp
+//  DataStructure
+//
+//  Created by Veliz, Luis on 4/12/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_h
+#define BinaryTreeTester_h
+
+#include "../Model/Nodes/BinaryTreeNode.hpp"
+#include "../Model/NonLinear/BinarySearchTree.hpp"
+
+class BinaryTreeTester
+{
+private:
+    BinaryTreeNode<int> testNode;
+    BinarySearchTree<int> testTree;
+public:
+    void treeStuff();
+};
+
+
+#endif /* BinaryTreeTester_h */
