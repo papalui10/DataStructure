85ea91da8c741037b6fdccd3df7b29926f15c1d4
Luis Veliz <luisvelizjr@gmail.com>
2019-04-16 12:07:10 -0600

    Created Tree class and BinaryTreeNode class.

---
+++ DataStructure/Model/NonLinear/BinarySearchTree.hpp
@@ -0,0 +1,39 @@
+//
+//  BinarySearchTree.hpp
+//  DataStructure
+//
+//  Created by Veliz, Luis on 4/16/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef BinarySearchTree_hpp
+#define BinarySearchTree_hpp
+
+#include "Tree.hpp"
+#include <iostream>
+
+using namespace std;
+
+template <class Type>
+class BinarySearchTree : public Tree<Type>
+{
+protected:
+    //MARK: Protected Methods
+    int calculateSize(BinaryTreeNode<Type> * startNode);
+    int calculateHeight(BinaryTreeNode<Type> * startNode);
+    bool isBalanced(BinaryTreeNode<Type> * startNode);
+    bool isComplete(BinaryTreeNode<Type> * postStart);
+    
+    void inOrderTraversal(BinaryTreeNode<Type> * inStart);
+    void preOrderTraversal(BinaryTreeNode<Type> * preStart);
+    void postOrderTraversal(BinaryTreeNode<Type> * postStart);
+    
+    void destroyTree(BinaryTreeNode<Type> * node);
+    
+    BinaryTreeNode<Type> * getRightMostChild(BinaryTreeNode<Type> * current);
+    BinaryTreeNode<Type> * getLeftMostChild(BinaryTreeNode<Type> * current);
+    
+    void removeNode(BinaryTreeNode<Type> * removeMe);
+}
+
+#endif /* BinarySearchTree_h */
